50d3c80208ad2d77ebc422f1a832b9b9c08d3472
root <root@patric.enderson>
2022-12-02 07:38:57 -0300

    Feat :: Criação de uma negociação

--- client/js/index.js
+++ client/js/index.js
@@ -4,5 +4,28 @@ var campos = [
   document.querySelector('#valor')
 ];
 
+var tbody = document.querySelector('table tbody')
 
-console.log(campos)
+document.querySelector('.form').addEventListener('submit', function (event) {
+
+  event.preventDefault();
+
+  var tr = document.createElement('tr');
+
+  campos.forEach(function (campo) {
+
+    var td = document.createElement('td');
+    td.textContent = campo.value;
+    tr.appendChild(td);
+
+  });
+
+  var tdVolume = document.createElement('td');
+
+  tdVolume.textContent = campos[1].value * campos[2].value;
+
+  tr.appendChild(tdVolume);
+  tbody.appendChild(tr)
+
+  console.log(tr)
+})
